14fe8c9661f56e44f8518135a9f4a7fecc511737
vivikari <vivivikaori@gmail.com>
2024-10-29 21:08:51 -0300

    jogo
    jogo

---
+++ coracao-da-amazonia-TD/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public TextMeshProUGUI waveCountdownText;
+
+    private int waveIndex = 0;
+
+    void Update ()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        waveCountdownText.text = Mathf.Ceil(countdown).ToString();
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave ()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
+
+    void SpawnEnemy ()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
