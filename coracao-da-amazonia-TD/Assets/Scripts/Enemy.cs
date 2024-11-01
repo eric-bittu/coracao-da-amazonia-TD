7e156c003cd0b9e1d555a0c350159a6d163ef44e
eric-bittu <eric.silva2@edu.fecap.br>
2024-11-01 08:45:29 -0300

    Hp do Jogador
    Feito o Hp do jogador

--- coracao-da-amazonia-TD/Assets/Scripts/Enemy.cs
+++ coracao-da-amazonia-TD/Assets/Scripts/Enemy.cs
@@ -4,45 +4,52 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    //velocidade do inimigo
+    //Velocidade do inimigo
     public float speed = 10.0f;
 
-    //objeto transform que indica o waypoint atual que o inimigo se dirige
+    //Objeto transform que indica o waypoint atual que o inimigo se dirige
     private Transform target;
-    //indice que rastreia o waypoint atual no array de waypoints
+    //Indice que rastreia o waypoint atual no array de waypoints
     private int wavepointIndex = 0;
-    
+    public GameObject hpPlayer;
     void Start()
     {
-        //define o primeiro waypoint como alvo para o inimigo se mover
+
+        hpPlayer = GameObject.Find("GameMaster");
+        
+
+        //Define o primeiro waypoint como alvo para o inimigo se mover
         target = Waypoints.points[0];
     }
     
     void Update()
     {
-        //calcula a direçao entre a posiçao atual do inimigo e o proximo waypoint
+        //Calcula a direçao entre a posiçao atual do inimigo e o proximo waypoint
         Vector3 dir = target.position - transform.position;
-        //move o inimigo na direçao com uma velocidade constante, ajustada por Time.deltaTime para movimento suave
+        //Move o inimigo na direçao com uma velocidade constante, ajustada por Time.deltaTime para movimento suave
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-        //verifica se o inimigo esta proximo o suficiente do waypoint atual para ir para o proximo waypoint
+        //Verifica se o inimigo esta proximo o suficiente do waypoint atual para ir para o proximo waypoint
         if (Vector3.Distance(transform.position, target.position) <= 0.4f)
         {
-            //se sim, chama o metodo
+            //Se sim, chama o metodo
             GetNextWaypoint();
         }
     }
 
     void GetNextWaypoint()
     {
-        //verifica se o inimigo chegou no ultimo waypoint
+        //Verifica se o inimigo chegou no ultimo waypoint
         if (wavepointIndex >= Waypoints.points.Length - 1)
         {
-            //se sim, signfica que completou o caminho e destroi o objeto
+            //Se sim, signfica que completou o caminho e destroi o objeto e dá dano
+            hpPlayer.GetComponent<HpPlayer>().TomarDano(1);
             Destroy(gameObject);
+            hpPlayer.GetComponent<HpPlayer>().Perder();
             return;
+            
         }
-        //se nao, define o proximo waypoint na lista como o novo alvo
+        //Se nao, define o proximo waypoint na lista como o novo alvo
         wavepointIndex++;
         target = Waypoints.points[wavepointIndex];
     }
