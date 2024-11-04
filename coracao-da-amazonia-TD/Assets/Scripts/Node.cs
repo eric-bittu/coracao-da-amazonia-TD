d619d3735fcf19c26e4028d117a943fd8c792092
vivikari <vivivikaori@gmail.com>
2024-11-04 16:18:53 -0300

    Movimento do personagem arqueiro
    Detecção do inimigo no range dele e movimento de olhar e seguir o inimigo

--- coracao-da-amazonia-TD/Assets/Scripts/Node.cs
+++ coracao-da-amazonia-TD/Assets/Scripts/Node.cs
@@ -11,31 +11,42 @@ public class Node : MonoBehaviour
 
     private Renderer rend;
     private Color startColor;
+
     void Start()
     {
+        //atribui Renderer do objeto a rend, para poder manipular cor do node
         rend = GetComponent<Renderer>();
+        //cor inicial do node
         startColor = rend.material.color;
     }
 
+    //quando o jogador clica no node
     void OnMouseDown()
     {
+        //verifica se tem objeto construido neste node
         if (arqueiro != null)
         {
+            //se sim, impede que outro objeto seja construido no mesmo lugar
             Debug.Log("Não da pra construir ai!");
             return;
         }
-
+        //prefab do arqueiro construido atraves do BuildManager
         GameObject arqueiroToBuild = BuildManager.instance.GetArqueiroToBuild();
+        //instancia o personagem no node, ajustando a posiçao com um positionOffset no ponto Y
         arqueiro = (GameObject)Instantiate(arqueiroToBuild, transform.position + positionOffset, transform.rotation);
     }
 
+    //quando o cursor do mouse passa sobre o node
     void OnMouseEnter()
     {
+        //altera a cor para indicar onde vai selecionar a construçao
         rend.material.color = hoverColor;
     }
 
+    //quando o cursor do mouse sai do node
     void OnMouseExit()
     {
+        //restaura a cor original
         rend.material.color = startColor;
     }
 }
